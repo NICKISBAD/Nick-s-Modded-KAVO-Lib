04294f6c01941c9493683196bcf461759ef82863
Nick <107813557+NICKISBAD@users.noreply.github.com>
2023-12-26 00:33:37 +0500

    Add files via upload

--- Nick'sModdedKavoLib.lua
+++ Nick'sModdedKavoLib.lua
@@ -245,40 +245,6 @@ end)
     headerCover.Name = "headerCover"
     headerCover.Parent = MainHeader
     
-local UserInputService = game:GetService("UserInputService")
-
-local dragging
-local dragInput
-local dragStart
-local startPos
-
-MainHeader.InputBegan:Connect(function(input)
-	if input.UserInputType == Enum.UserInputType.Touch then
-		dragging = true
-		dragStart = input.Position
-		startPos = MainHeader.Position
-
-		input.Changed:Connect(function()
-			if input.UserInputState == Enum.UserInputState.End then
-				dragging = false
-			end
-		end)
-	end
-end)
-
-MainHeader.InputChanged:Connect(function(input)
-	if input.UserInputType == Enum.UserInputType.Touch then
-		dragInput = input
-	end
-end)
-
-UserInputService.InputChanged:Connect(function(input)
-	if input == dragInput and dragging then
-		local delta = input.Position - dragStart
-		MainHeader.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
-	end
-end)
-
     coverup.Name = "coverup"
     coverup.Parent = MainHeader
     coverup.BackgroundColor3 = themeList.Header
